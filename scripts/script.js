75faec9630ae17b01d26db2960cbcfcefe02a14e
Alexander Dolonsky <dolonskiy2011@yandex.ru>
2021-11-17 01:46:50 +0300

    feat; Исправить ошибки после первой итерации

--- scripts/script.js
+++ scripts/script.js
@@ -4,29 +4,28 @@ const popupCloseButton = popup.querySelector(".popup__close");
 const popupOverlay = popup.querySelector(".popup__overlay");
 const popupSave = popup.querySelector(".popup__save");
 const popupForm = popup.querySelector(".popup__form");
-const nameInput = popupForm.querySelector(".popup__name");
-const aboutInput = popupForm.querySelector(".popup__about");
+const nameInput = popupForm.querySelector(".popup__edit_name");
+const aboutInput = popupForm.querySelector(".popup__edit_about");
+const nameOutput = document.querySelector(".profile__name");
+const aboutOutput = document.querySelector(".profile__about");
 
 function open() {
-    popup.classList.add("popup__opened");
+    popup.classList.add("popup_opened");
+    nameOutput.textContent = nameInput.value;
 }
 
 function close() {
-    popup.classList.remove("popup__opened");
-}
-
-if (aboutLink) {
-    aboutLink.addEventListener('click', open);
+    popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    const Name = document.querySelector(".profile__name");
-    const About = document.querySelector(".profile__about");
-    Name.textContent = nameInput.value
-    About.textContent = aboutInput.value
-    popup.classList.remove("popup__opened");
+    nameOutput.textContent = nameInput.value;
+    aboutOutput.textContent = aboutInput.value;
+    popup.classList.remove("popup_opened");
 }
+
+aboutLink.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
 popupOverlay.addEventListener('click', close);
-popupSave.addEventListener('click', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler);
